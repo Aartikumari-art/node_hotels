0ac6aae62ffe5eeae025bc41f7d31c442d5a6276
Aarti Kumari <kaarti.kc@gmail.com>
2024-07-12 11:11:50 +0530

    changes shift from mongidb to online server

--- db.js
+++ db.js
@@ -1,51 +1,58 @@
-const mongoose = require('mongoose');
 
-require('dotenv').config();
+// require('dotenv').config();
+// const mongoose = require('mongoose');
 //difine mongodb connection
 
 //const mongoURL = 'mongodb://localhost:27017/hotels'
+//const mongoURL = process.env.MONGODB_URL;
+
+//mongodb+srv://HelloNode:HelloNode12345@cluster0.tionq7f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+// const  mongoURL = process.env.ATLASH_DB;
+
+// mongoose.connect(mongoURL);
 
 
 
-const  mongoURL = process.env.MONGODB_URL;
 
-mongoose.connect(mongoURL);
- //set up mongoDB connection for local hosting
 
- //const mongoURL = 'mongodb+srv://HelloNode:HelloNode12345@cluster0.tionq7f.mongodb.net/'
-  ////Global hosting of our database
+
+// MongoDB connection string
+//  
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const mongoURL = process.env.ATLASH_DB;
+
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tlsAllowInvalidCertificates: true // For development only
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+module.exports = mongoose.connection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  
-  
-  
-  
-  
-  
-// const uri = "mongodb+srv://HelloNode:HelloNode12345@cluster0.mongodb.net/menu?retryWrites=true&w=majority";
 
-// mongoose.connect(uri)
-// .then(() => {
-//     console.log('Connected to MongoDB');
-// }).catch(err => {
-//     console.error('Error connecting to MongoDB', err);
-// });
-
- 
-
-
-
- const db=mongoose.connection;
- //define event listener for database connetion
-
- db.on('connected',()=>{
-    console.log('connected to mongoDB  server');
- });
-
- db.on('error',(err)=>{
-    console.log(' mongoDB connection error',err);
- });
- db.on('disconnected',()=>{
-    console.log(' mongodb disconnected ');
- });
-
- module.exports = db; 
+module.exports = mongoose.connection;
